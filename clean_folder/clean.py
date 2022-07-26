d329a5f9c866c0aa8a29af16a58c8ccec285d464
VadimChubar <chvagr@gmail.com>
2022-07-26 13:22:57 +0300

    Update clean.py

--- clean_folder/clean.py
+++ clean_folder/clean.py
@@ -1,36 +1,26 @@
 from pathlib import Path
 import shutil
 import sys
-import file_processing as parser
+from clean_folder import file_processing
 import re
-
 
 CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
-
 TRANS = {}
 for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
     TRANS[ord(c)] = l
     TRANS[ord(c.upper())] = l.upper()
-
-
 def normalize(name: str) -> str:
     t_name = name.translate(TRANS)
     t_name = re.sub(r'\W', '_', t_name)
     return t_name
-
-
 def handle_media(filename: Path, target_folder: Path):
     target_folder.mkdir(exist_ok=True, parents=True)
     filename.replace(target_folder / normalize(filename.name))
-
-
 def handle_other(filename: Path, target_folder: Path):
     target_folder.mkdir(exist_ok=True, parents=True)
     filename.replace(target_folder / normalize(filename.name))
-
-
 def handle_archive(filename: Path, target_folder: Path):
     # Створюємо папку для архіву
     target_folder.mkdir(exist_ok=True, parents=True)
@@ -38,7 +28,6 @@ def handle_archive(filename: Path, target_folder: Path):
     # Беремо суфікс у файла і удаляємо replace(filename.suffix, '')
     folder_for_file = target_folder / \
         normalize(filename.name.replace(filename.suffix, ''))
-
     # Створюємо папку для архіву з іменем файлу
     folder_for_file.mkdir(exist_ok=True, parents=True)
     try:
@@ -49,39 +38,32 @@ def handle_archive(filename: Path, target_folder: Path):
         folder_for_file.rmdir()
         return None
     filename.unlink()
-
-
 def handle_folder(folder: Path):
     try:
         folder.rmdir()
     except OSError:
         print(f'Error delete folder: {folder}')
-
-
-def main(folder: Path):
-    parser.scan(folder)
-    for file in parser.IMAGES:
-        handle_media(file, folder / 'images')
-    for file in parser.DOCUMENTS:
-        handle_media(file, folder / 'documents')
-    for file in parser.AUDIO:
-        handle_media(file, folder / 'audio')
-    for file in parser.VIDEO:
-        handle_media(file, folder / 'video')
-
-    for file in parser.MY_OTHER:
-        handle_other(file, folder / 'my_other')
-
-    for file in parser.ARCHIVES:
-        handle_archive(file, folder / 'archives')
-
+def main():
+    folder_for_scan = Path(sys.argv[1])
+    print(f'Start in folder {folder_for_scan.resolve()}')
+    file_processing.scan(folder_for_scan)
+    for file in file_processing.IMAGES:
+        handle_media(file, folder_for_scan / 'images')
+    for file in file_processing.DOCUMENTS:
+        handle_media(file, folder_for_scan / 'documents')
+    for file in file_processing.AUDIO:
+        handle_media(file, folder_for_scan / 'audio')
+    for file in file_processing.VIDEO:
+        handle_media(file, folder_for_scan / 'video')
+    for file in file_processing.MY_OTHER:
+        handle_other(file, folder_for_scan / 'my_other')
+    for file in file_processing.ARCHIVES:
+        handle_archive(file, folder_for_scan / 'archives')
     # Виконуємо реверс списку для того щоб видалити всі папки
-    for folder in parser.FOLDERS[::-1]:
+    for folder in file_processing.FOLDERS[::-1]:
         handle_folder(folder)
-
-
-if __name__ == '__main__':
-    if sys.argv[1]:
-        folder_for_scan = Path(sys.argv[1])
-        print(f'Start in folder {folder_for_scan.resolve()}')
-        main(folder_for_scan.resolve())
+# if __name__ == '__main__':
+#     if sys.argv[1]:
+#         folder_for_scan = Path(sys.argv[1])
+#         print(f'Start in folder {folder_for_scan.resolve()}')
+#         main(folder_for_scan.resolve())
